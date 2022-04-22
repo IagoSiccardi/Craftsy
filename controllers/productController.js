db045e6d605c2bc97a00e8bc9d019c253e95973d
IagoSiccardi <iago.siccardi@gmail.com>
2022-04-22 11:30:10 -0300

    rutas dinamicas

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,10 +1,30 @@
+const categories = require ('../data/categories');
+const products = require('../data/products');
+
+
+
 module.exports = {
-    detail : (req,res) =>{
+    detail : (req,res) => {
+
         const {id} = req.params;
+        const products = products.find(product => product.id === +id.product)
         
         res.render ('productDetail',{
-            numberImage : id
+            product
         })
     },
-    cart : (req,res) => res.render ('productCart')
+    cart : (req,res) => res.render ('productCart'),
+
+    getByCategory : (req,res) => {
+
+        const {idCategory} = req.params
+
+        const {name,products} = categories.find (category => category.id === +idCategory)
+
+        return res.render('categories',{
+            name,
+            products
+        })
+        
+    }
 }
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const {cart,detail} = require ('../controllers/productController')
+const {cart,detail, getByCategory,} = require ('../controllers/productController')
 
 /* GET users listing. */
 router.get('/cart', cart);
 router.get ('/detail/:id', detail)
+router.get('/category/:idCategory/:idProduct?',getByCategory)
 
 module.exports = router;
+ 
